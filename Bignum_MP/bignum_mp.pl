47587347818d5c72e3986e6e87bde795d2921b2a
Vulkan <523066680@163.com>
2018-12-18 15:49:03 +0800

    初步实现

--- Bignum_MP/bignum_mp.pl
+++ Bignum_MP/bignum_mp.pl
@@ -3,7 +3,8 @@ use List::Util qw/max/;
 STDOUT->autoflush(1);
 
 my $num_a = 123456;
-my $num_b = 654321;  #single num
+my $num_b = 9999;  #single num
+printf "%d\n", $num_a * $num_b;
 
 my @ele_a = reverse split("", $num_a );
 my @ele_b = reverse split("", $num_b );
@@ -27,6 +28,7 @@ for my $eb ( @ele_b )
 
         if (defined $buff[$sid+1]) { $buff[$sid+1] += $foo; }
         else  { $buff[$sid+1] = $foo; }
+
         $sid++;
     }
     $id++;
@@ -36,6 +38,18 @@ for my $id ( 0 .. $#buff )
 {
     printf "%s%s\n", " "x$id, scalar reverse( "${buff[$id]}");
 }
+
+MERGE: #合并
+for my $id ( 0 .. $#buff )
+{
+    if ( $buff[$id] >= 10 )
+    {
+        $buff[$id+1] += int($buff[$id] / 10);
+        $buff[$id] = $buff[$id] % 10;
+    }
+}
+
+printf "%s\n", join("", reverse @buff);
 
 
 
