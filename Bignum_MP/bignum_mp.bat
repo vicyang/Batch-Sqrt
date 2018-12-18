a922d9b50289c9743abad76e29cbfd259c7367a7
Vulkan <523066680@163.com>
2018-12-18 16:43:00 +0800

    批处理大数乘法 初步实现

---
+++ Bignum_MP/bignum_mp.bat
@@ -0,0 +1,57 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:init
+  rem static template for calculating strlen
+  set "mod="
+  set /a maxlen = 1000
+  for /l %%a in (1,1,%maxlen%) do set mod=!mod!#
+
+
+set num_a=123456
+set num_b=9999
+set num_a=1234569999999999999999999999999999999999999999999999999999999999
+set num_b=9999999999999999999999999999999999999999999999999999999999999999999999999999999
+
+:bignum_mp
+    echo %time%
+    set stock=0
+    call :length %num_a% len_a
+    call :length %num_b% len_b
+    for /l %%a in (0, 1, %attemplen%) do set buff[%%a]=0
+    set /a id = 0, sid = 0, maxid = 0
+    for /l %%b in ( 1, 1, %len_b% ) do (
+        set /a sid = id
+        for /l %%a in ( 1, 1, %len_a% ) do (    
+            set /a next = sid + 1
+            set /a ea = !num_a:~-%%a,1!, eb = !num_b:~-%%b,1!
+            set /a mp = ea * eb, foo = mp/10, bar = mp %% 10
+            rem echo foo:!foo! bar:!bar!
+            set /a buff[!sid!] += bar, buff[!next!] += foo
+            set /a sid += 1, maxid = sid
+        )
+        set /a id += 1
+    )
+    echo %time%
+
+    :merge
+        set /a id = 0
+        for /l %%c in ( 0, 1, %maxid% ) do (
+            set /a next = %%c+1
+            set /a buff[!next!] += buff[%%c]/10
+            set /a buff[%%c] = buff[%%c] %% 10
+        )
+
+    for /l %%a in (%maxid%, -1, 0) do set /p inp="!buff[%%a]!"<nul
+    goto :eof
+    
+:length %str% %vname%
+    setlocal
+    set test=%1_%mod%
+    set test=!test:~0,%maxlen%!
+    set test=%test:*_=%
+    set /a len=maxlen-(%test:#=1+%1)
+    endlocal &set %2=%len%
+    goto :eof
+
+
