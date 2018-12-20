6df8e8d74b17181250ead0f47fb91d7ee3e1ddd5
Vulkan <523066680@163.com>
2018-12-20 10:36:39 +0800

    添加时间计算函数，求积代码函数化

--- Bignum_MP/bignum_mp.bat
+++ Bignum_MP/bignum_mp.bat
@@ -3,8 +3,8 @@ setlocal enabledelayedexpansion
 
 :init
   rem static template for calculating strlen
-  set "mod=" &set /a maxlen = 1000
-  for /l %%a in (1,1,%maxlen%) do set mod=!mod!#
+  set "mod=" &set /a maxlen=2000, half=maxlen/2
+  for /l %%a in (1,1,%half%) do set mod=!mod!##
 
 set num_a=123456
 set num_b=9999
@@ -13,9 +13,15 @@ set num_b=9999999999999999999999999999999999999999999999999999999999999999999999
 
 rem set num_a=100
 rem set num_b=1000
+call :bignum_mp %num_a% %num_b% product
+echo %product%
+exit
 
 :bignum_mp
-    echo %time%
+    setlocal
+    set time_a=%time%
+    set num_a=%1
+    set num_b=%2
     call :length %num_a% len_a
     call :length %num_b% len_b
     for /l %%b in ( 1, 1, %len_b% ) do ( set ele_b=!ele_b! !num_b:~-%%b,1! )
@@ -46,8 +52,10 @@ rem set num_b=1000
         )
 
     if "!buff[%maxid%]!" == "0" set /a maxid-=1
-    for /l %%a in (%maxid%, -1, 0) do set /p inp="!buff[%%a]!"<nul
-    echo,&echo %time%
+    set product=
+    for /l %%n in (%maxid%, -1, 0) do set product=!product!!buff[%%n]!
+    call :time_used %time_a% %time%
+    endlocal &set %3=%product%
     goto :eof
     
 :length %str% %vname%
@@ -59,4 +67,17 @@ rem set num_b=1000
     endlocal &set %2=%len%
     goto :eof
 
-
+:time_used %time_a% %time_b%
+    rem only for few seconds, not consider minutes
+    setlocal
+    set ta=%1& set tb=%2
+    set ta=#%ta:~-5%& set tb=#%tb:~-5%
+    set ta=%ta:#0=%& set tb=%tb:#0=%
+    set ta=%ta:#=%& set tb=%tb:#=%
+    set /a dt = %tb:.=% - %ta:.=%
+    set dt=%dt:-=%
+    set dt=0000%dt%
+    set dt=%dt:~-4%
+    echo time used: %dt:~0,2%.%dt:~2,2%s
+    endlocal
+    goto :eof
