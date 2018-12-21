17ae29f390ebc1b17b9ff4c4f4a2374523719635
Vulkan <523066680@163.com>
2018-12-21 13:00:50 +0800

    all is fine

--- Binary_Search/BinSearch.bat
+++ Binary_Search/BinSearch.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 set echo=rem
-for /l %%a in (0,1,100) do (
+for /l %%a in (0,1,99) do (
     call :get_int_of_root %%a root cmp
     perl -e "printf qq(num=%%3d r=%%2d f=%%4.1f cmp=%%2s\n), %%a, !root!, sqrt(%%a), !cmp!"
 )
@@ -21,7 +21,7 @@ exit
     setlocal
     set /a num = %1
     call :length %num% len
-    set /a min = 1, max = 9, root_len = len / 2 + len %% 2
+    set /a min = 0, max = 10, root_len = len / 2 + len %% 2
     for /l %%n in (2,1,%root_len%) do set min=!min!0&set max=!max!9
     set /a mid = (min+max)/2
     set /a quit = 0
@@ -31,7 +31,7 @@ exit
         if %product% equ %num% set /a quit = 1, cmp=0
         if %product% gtr %num% set /a max = mid, cmp=1
         if %product% lss %num% set /a min = mid, cmp=-1
-        if %range% leq 1 (set quit=1)
+        if %range% leq 1 set /a quit = 1
         %echo% tg=%num% pow=%product% min:%min% max:%max% mid:%mid% %cmp%
         set /a mid=(min+max)/2
     if %quit% == 0 goto :binary_search
