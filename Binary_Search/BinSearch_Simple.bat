3a81c400570f2ef3884b58e71ad16b60ce6a6d6c
Vulkan <523066680@163.com>
2018-12-21 11:30:44 +0800

    二分搜索简单测试。

---
+++ Binary_Search/BinSearch_Simple.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for /l %%a in (1,1,9) do (
+    call :bin_search %%a check
+    echo %%a - !check!
+)
+exit /b
+
+
+:bin_search
+    setlocal
+    set /a expect = %1
+    set /a min = 1, max = 9, mid = (min+max)/2
+    set /a quit = 0
+
+    :loop
+        set /p inp="min=%min% max=%max% mid=%mid%"<nul
+        if %mid% equ %expect% set /a quit=1, cmp=0
+        if %mid% gtr %expect% set /a max=mid-1, mid=(max+min)/2, cmp=1
+        if %mid% lss %expect% set /a min=mid+1, mid=(max+min)/2, cmp=-1
+        if %max% leq %min% (set quit=1)
+        echo  %cmp%
+    if %quit% == 0 goto :loop
+    echo min=%min% max=%max% mid=%mid% %cmp%
+
+    endlocal&set %2=%mid%
+    goto :eof
