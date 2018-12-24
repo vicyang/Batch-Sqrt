a33f5015343cb9783fbbd1672f2d89f3e317bfca
Vulkan <523066680@163.com>
2018-12-24 15:39:47 +0800

    格式、变量名展开

--- Decimal-Method/C_from_network.c
+++ Decimal-Method/C_from_network.c
@@ -5,34 +5,38 @@
 #include <stdio.h>
 #include <string.h>  
   
-int l;  
-int work(int o,char *O,int I)  
-{  
-    char c, *D=O ;  
-    if(o>0)  
-    {  
-        for(l=0;D[l];D[l++]-=10)  
-        {  
-            D[l++]-=120;  
-            D[l]-=110;  
-            while(!work(0,O,l))  
-                D[l]+=20;  
-            putchar((D[l]+1032)/20);  
-        }  
-        putchar(10);  
-    }  
-    else  
-    {  
-        c=o+(D[I]+82)%10-(I>l/2)*(D[I-l+I]+72)/10-9;  
-        D[I]+=I<0 ? 0 : !(o=work(c/10,O,I-1))*((c+999)%10-(D[I]+92)%10);  
+int id;
+int work(int o, char *num, int ID)
+{
+    char c, *tnum=num ;
+    if( o > 0 )
+    {
+        for ( id = 0; tnum[id]; id++ )
+        {
+            tnum[ id++ ]-=120;
+            tnum[ id ]-=110;
+            while ( !work(0, num, id) )
+            {
+                tnum[id]+=20;
+            }
+            putchar( (tnum[id]+1032)/20 );
+            tnum[id] -= 10;
+        }
+        putchar(10);
+    }
+    else
+    {
+        c = o + (tnum[ID]+82)%10 - (ID>id/2) * (tnum[ID-id+ID]+72)/10-9;
+        tnum[ID]+=ID < 0 ? 0 : !( o = work(c/10, num, ID-1) ) * ( (c+999)%10-(tnum[ID]+92) % 10 );
     }  
     return o;  
-}  
-  
+}
+
 int main()  
 {  
-    char s[1200];s[0]='0';  
-    scanf("%s",s+1);  
+    char s[1200];
+    s[0]='0';
+    sscanf("123456787654321", "%s", s+1);
     if(strlen(s)%2 == 1)  
         work(2,s+1,0);  
     else  
