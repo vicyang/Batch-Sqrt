2e8d6cba41b9c477d864dc83ed736a95f343ef86
Vulkan <523066680@163.com>
2018-12-25 19:38:38 +0800

    精简代码，临时保存

--- Bignum_MP/bignum_mp.pl
+++ Bignum_MP/bignum_mp.pl
@@ -6,50 +6,50 @@ my $num_a = 123456;
 my $num_b = 9999;  #single num
 printf "%d\n", $num_a * $num_b;
 
-my @ele_a = reverse split("", $num_a );
-my @ele_b = reverse split("", $num_b );
+bignum_mp( $num_a, $num_b );
 
-my @buff;
-my ($foo, $bar, $mp);
-my ( $id, $sid ) = (0, 0);
-my $t;
-
-for my $eb ( @ele_b )
+sub bignum_mp
 {
-    $sid = $id;
-    for my $ea (@ele_a)
-    {
-        $mp = $ea * $eb;
-        $foo = int( $mp/10 );
-        $bar = $mp % 10;
+    my ($num_a, $num_b) = @_;
+    my @buff;
+    my $sid;
 
-        if (defined $buff[$sid]) { $buff[$sid] += $bar; }
-        else  { $buff[$sid] = $bar; }
-
-        if (defined $buff[$sid+1]) { $buff[$sid+1] += $foo; }
-        else  { $buff[$sid+1] = $foo; }
-
-        $sid++;
+    for my $ia ( 1 .. length($num_a) ) {
+        $sid = $ia-1;
+        for my $ib ( 1 .. length($num_b) ) {
+            $buff[$sid++] += substr($num_a, -$ia, 1) * substr($num_b, -$ib, 1);
+        }
     }
-    $id++;
-}
 
-for my $id ( 0 .. $#buff )
-{
-    printf "%s%s\n", " "x$id, scalar reverse( "${buff[$id]}");
-}
 
-MERGE: #合并
-for my $id ( 0 .. $#buff )
-{
-    if ( $buff[$id] >= 10 )
+    # for my $eb ( @ele_b )
+    # {
+    #     $sid = $id;
+    #     for my $ea ( @ele_a )
+    #     {
+    #         $buff[$sid] += $ea * $eb;
+    #         $sid++;
+    #     }
+    #     $id++;
+    # }
+
+    for my $id ( 0 .. $#buff )
     {
-        $buff[$id+1] += int($buff[$id] / 10);
-        $buff[$id] = $buff[$id] % 10;
+        printf "%s%s\n", " "x$id, scalar reverse( "${buff[$id]}");
     }
-}
 
-printf "%s\n", join("", reverse @buff);
+    MERGE: #合并
+    for my $id ( 0 .. $#buff )
+    {
+        if ( $buff[$id] >= 10 )
+        {
+            $buff[$id+1] += int($buff[$id] / 10);
+            $buff[$id] = $buff[$id] % 10;
+        }
+    }
+
+    printf "%s\n", join("", @buff);
+}
 
 
 
