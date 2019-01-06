968469342d1ad2e9b34d9194539cbee9eef88b8a
Vulkan <523066680@163.com>
2019-01-06 12:33:19 +0800

    bignum_mp_single 细节优化。删除冗余代码，未做详细校验。

--- Bignum_MP/bignum_mp_SingleNum.bat
+++ Bignum_MP/bignum_mp_SingleNum.bat
@@ -2,22 +2,29 @@
 setlocal enabledelayedexpansion
 
 :init
-  rem static template for calculating strlen
-  set "mod="
-  set /a maxlen = 2000, half = maxlen/2
-  for /l %%a in (1,1,%half%) do set mod=!mod!##
+    rem static template for calculating strlen
+    set "sharp=#"
+    set /a pow=11, maxlen=1^<^<pow
+    for /l %%a in (1,1,%pow%) do set sharp=!sharp!!sharp!
 
-set num_a=2
+set num_a=1999999999
 set num_b=9
 
-for /l %%a in (1,1,1000) do (set num_a=!num_a!2)
+for /l %%a in (1,1,2000) do (set num_a=!num_a!9)
+
+call :check_first %num_a%*%num_b%
 
 set ta=%time%
-call :bignum_mp_single %num_a% %num_b% product
+call :bignum_mp_single %num_a% %num_b% mp mp_len
 call :time_delta %ta% %time% tu
-echo %product%
+echo %mp%
+echo len: %mp_len%
 echo time used: %tu%
 exit
+
+:check_first
+    perl -Mbignum -le "print %1" 2>nul
+    goto :eof
 
 :bignum_mp_single
     setlocal
@@ -27,26 +34,19 @@ exit
     set /a pool = 0, maxid = len
     for /l %%a in ( 1, 1, %len% ) do (
         set /a mp = !num_a:~-%%a,1! * num_b + pool
-        if !mp! geq 10 (
-            set /a buff[%%a] = mp %% 10, pool = mp / 10
-        ) else (
-            set /a buff[%%a] = mp
-        )
+        set /a buff[%%a] = mp %% 10, pool = mp / 10
     )
 
-    if %pool% neq 0 (
-        set /a maxid+=1
-        set /a buff[!maxid!] = pool
-    )
+    if %pool% neq 0 set /a maxid+=1, buff[!maxid!] = pool
 
     set res=
     for /l %%n in (%maxid%, -1, 0) do set res=!res!!buff[%%n]!
-    endlocal&set %3=%res%
+    endlocal&set %3=%res%&set %4=%maxid%
     goto :eof
     
 :length %str% %vname%
     setlocal
-    set test=%1_%mod%
+    set test=%1_%sharp%
     set test=!test:~0,%maxlen%!
     set test=%test:*_=%
     set /a len=maxlen-(%test:#=1+%1)
