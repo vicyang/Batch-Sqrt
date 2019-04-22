aaf32434ed23db157c0fe2ac02c25662e4301f54
Vulkan <523066680@163.com>
2019-04-22 09:15:20 +0800

    Signed-off-by: Vulkan <523066680@163.com>

--- Bignum_Plus/benchmark/check.pl
+++ Bignum_Plus/benchmark/check.pl
@@ -3,16 +3,17 @@ use bigint 'a'=>9000;
 STDOUT->autoflush(1);
 
 srand(1);
-for (1..50) { check() }
+for (1..50) { check("hongrk4.bat") }
 
 sub check
 {
+    my ($script) = @_;
     my $a = int(rand(9))+1 .join "", map { int(rand(10)) } ( 1 .. rand(4100) );
     my $b = int(rand(9))+1 .join "", map { int(rand(10)) } ( 1 .. rand(4100) );
 
     if ( length($b) > length($a) ) { ($a, $b) = ($b, $a); }
     my $ta = time();
-    my $res = `plus_vec4.bat $a $b`;
+    my $res = `$script $a $b 2>nul`;
     #my $res = `hongrk3.bat $a $b`;
     my $dt = time()-$ta;
     my $check = (0+$a)+$b;
